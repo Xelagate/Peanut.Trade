e4eea4c024856c53084f11b6bbcdd647a1b8d096
Alexandr Pylypenko <88944379+Xelagate@users.noreply.github.com>
2025-02-04 11:57:11 +0200

    Update binance.py

--- app/services/binance.py
+++ app/services/binance.py
@@ -5,11 +5,12 @@ async def get_binance_rate(base_currency: str, quote_currency: str) -> float:
     Получает обменный курс `base_currency` → `quote_currency` с Binance.
     """
     symbol = f"{base_currency}{quote_currency}".upper()
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
+    url = f"https://api.binance.us/api/v3/ticker/price?symbol={symbol}"
 
     try:
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
+            response.raise_for_status()
             data = response.json()
 
             if "price" in data:
